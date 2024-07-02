ce96796c2d824cf7d2119a6fec739f43040970b6
wangshouren <1115808717@qq.com>
2024-07-02 12:51:26 +0800

    chore: use pm2 to start server

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "large-file-upload", // Name of your application
+      script: "npm", // Command to execute
+      args: "start", // Arguments to pass to the command
+    },
+  ],
+};
