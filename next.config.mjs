fa5ebbdecab13fc98aa7189155de1eb54c4df8df
wangshouren <1115808717@qq.com>
2024-07-03 20:10:47 +0800

    chore: ignore eslint in build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.BASE_PATH,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
